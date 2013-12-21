f490d1262d3352642021c3ca27f2fbd1812b248d
Walter Tetzner <walter@waltertetzner.net>
2013-12-21 12:24:57 -0500

    Started experimental design.

---
+++ experimental/scripts/libraries/mori.js
@@ -0,0 +1,2 @@
+(function(definition){if(typeof exports==="object"){module.exports=definition();}else if(typeof define==="function"&&define.amd){define(definition);}else{mori=definition();}})(function(){return function(){
+;return this.mori;}.call({});});
